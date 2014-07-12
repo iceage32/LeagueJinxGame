c5ad9927724f730b19006dad6728573ce9f0c874
iceage32 <zsozso5800@gmail.com>
2014-07-12 17:16:27 +0300

    Random commit

--- ice.js
+++ ice.js
@@ -1,7 +1,8 @@
 var Ice = {
     stage: null,
+    bg: null,
     spritesheet: null,
-    animation: null,
+    player: null,
     runningAnimation: false,
     KEYS: {
         LEFT: 37,
@@ -18,17 +19,24 @@ var Ice = {
     bullets: new createjs.Container(),
     bulletTime: 250,
     lastBulletTime: null,
+    monsters: new createjs.Container(),
+    monsterSpawnTime: 6000,
+    monsterLastSpawn: null,
+    monsterSpeed: 0.5,
 
     init: function(canvas) {
         Ice.stage = new createjs.Stage(canvas);
         Ice.spritesheet = new createjs.SpriteSheet(Ice.characters);
-        Ice.animation = new createjs.Sprite(Ice.spritesheet, 'run');
-        Ice.animation.gotoAndPlay('stand');
-        Ice.animation.x = 0;
-        Ice.animation.y= 0;
+        Ice.player = new createjs.Sprite(Ice.spritesheet, 'stand');
 
-        Ice.stage.addChild(Ice.animation);
-        Ice.stage.addChild(Ice.bullets);
+        //init and position background
+        Ice.bg = new createjs.Bitmap('assets/bg.png');
+        Ice.bg.x = Ice.stage.canvas.width - Ice.bg
+
+        Ice.player.x = 0;
+        Ice.player.y= 0;
+
+        Ice.stage.addChild(Ice.bg, Ice.player, Ice.bullets, Ice.monsters);
         createjs.Ticker.setFPS(60);
         createjs.Ticker.addEventListener("tick", Ice.handleTick);
 
@@ -37,10 +45,10 @@ var Ice = {
     },
     handleTick: function(e) {
         if(Ice.KEYHELD.up) {
-            Ice.animation.y -= 5;
+            Ice.player.y -= 5;
         }
         if(Ice.KEYHELD.down) {
-            Ice.animation.y += 5;
+            Ice.player.y += 5;
         }
         if(Ice.KEYHELD.ctrl) {
             if(Ice.lastBulletTime + Ice.bulletTime <= createjs.Ticker.getTime()) {
@@ -48,30 +56,64 @@ var Ice = {
             }
         }
 
-        var playerBounds = Ice.animation.getBounds();
-        if(Ice.animation.y <= 0) {
-            Ice.animation.y = 0;
+        var playerBounds = Ice.player.getBounds();
+        if(Ice.player.y <= 0) {
+            Ice.player.y = 0;
         }
-        if(Ice.animation.y >= Ice.stage.canvas.height - playerBounds.height) {
-            Ice.animation.y = Ice.stage.canvas.height - playerBounds.height;
+        if(Ice.player.y >= 500 - playerBounds.height) {
+            Ice.player.y = 500 - playerBounds.height;
+        }
+
+        //do monster thing
+        if(Ice.monsterSpawnTime + Ice.monsterLastSpawn <= createjs.Ticker.getTime()) {
+            Ice.spawnMonsters();
+            Ice.monsterLastSpawn = createjs.Ticker.getTime();
+        }
+
+        for(var i = 0; i< Ice.monsters.children.length; i++) {
+            var m = Ice.monsters.children[i];
+            m.x -= Ice.monsterSpeed;
+            if(m.x <= playerBounds.x) {
+                Ice.monsters.removeChild(m);
+            }
+
+            //check if hit monster
+            for(var k=0; k<Ice.bullets.children.length; k++) {
+                var b =  Ice.bullets.children[k];
+                var pt = b.localToLocal(0, 0, m);
+                if(m.hitTest(pt.x, pt.y)) {
+                    Ice.bullets.removeChild(b);
+                    Ice.monsters.removeChild(m);
+                }
+            }
         }
 
         //fire bullets
         for(var i=0; i<Ice.bullets.children.length; i++) {
-            Ice.bullets.children[i].x += 10;
-            if(Ice.bullets.children[i].x >= Ice.stage.canvas.width) {
-                Ice.bullets.removeChildAt(i);
+            var o = Ice.bullets.children[i];
+            o.x += 10;
+            if(o.x >= Ice.stage.canvas.width) {
+                Ice.bullets.removeChild(o);
             }
         }
 
+
         document.getElementById('fps').innerHTML = Math.round(createjs.Ticker.getMeasuredFPS());
+        Ice.stage.update();
+    },
+    spawnMonsters: function() {
+        var m = new createjs.Shape();
+        m.graphics.beginFill('blue').drawRect(0,0, 50, 50);
+        m.x = Ice.stage.canvas.width-50;
+        m.y = Ice.randomRange(0, 500-50);
+        Ice.monsters.addChild(m);
         Ice.stage.update();
     },
     shot: function() {
         var s = new createjs.Shape();
         s.graphics.beginFill('red').drawCircle(0, 0, 5);
-        s.y = Ice.animation.y + 25;
-        s.x = Ice.animation.x + 50;
+        s.y = Ice.player.y + 25;
+        s.x = Ice.player.x + 50;
         Ice.bullets.addChild(s);
         Ice.stage.update();
         Ice.lastBulletTime = createjs.Ticker.getTime();
@@ -80,25 +122,25 @@ var Ice = {
         var keyCode = e.which ? e.which: e.keyCode;
         if(keyCode == Ice.KEYS.DOWN) {
             Ice.KEYHELD.down = true;
-            if(!Ice.runningAnimation) {Ice.animation.gotoAndPlay('run');}
+            if(!Ice.runningAnimation) {Ice.player.gotoAndPlay('run');}
             Ice.runningAnimation = true;
 
         }
         if(keyCode == Ice.KEYS.UP) {
             Ice.KEYHELD.up = true;
-            if(!Ice.runningAnimation) {Ice.animation.gotoAndPlay('run'); }
+            if(!Ice.runningAnimation) {Ice.player.gotoAndPlay('run'); }
             Ice.runningAnimation = true;
         }
         if(keyCode == Ice.KEYS.CTRL) {
             Ice.KEYHELD.ctrl = true;
-            Ice.animation.gotoAndPlay('hit')
-            setTimeout(function() {Ice.animation.gotoAndStop((Ice.moving ? 'run':'stand'));}, 500);
+            Ice.player.gotoAndPlay('hit')
+            setTimeout(function() {Ice.player.gotoAndPlay(((Ice.KEYHELD.up || Ice.KEYHELD.down) ? 'run':'stand'));}, 500);
         }
     },
     handleKeyUp: function(e) {
         var keyCode = e.which ? e.which: e.keyCode;
         if(keyCode == Ice.KEYS.DOWN || keyCode == Ice.KEYS.UP) {
-            if(Ice.runningAnimation) {Ice.runningAnimation = false; Ice.animation.gotoAndPlay('stand');}
+            if(Ice.runningAnimation) {Ice.runningAnimation = false; Ice.player.gotoAndPlay('stand');}
         }
         if(keyCode == Ice.KEYS.UP) {
             Ice.KEYHELD.up = false;
@@ -141,5 +183,8 @@ var Ice = {
                 frames:[8, 9], speed:0.1, next:true
             }
         }
+    },
+    randomRange: function(min, max) {
+        return Math.floor(Math.random() * (max-min+1)) +min;
     }
 }
